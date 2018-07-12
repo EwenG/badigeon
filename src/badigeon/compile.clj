26bf63d3d4a09f511202550eae04e058d8d027db
ewen <ewen.grosjean@free.fr>
2018-07-13 00:14:40 +0200

    compile in a separate classloader

--- src/badigeon/compile.clj
+++ src/badigeon/compile.clj
@@ -1,10 +1,11 @@
 (ns badigeon.compile
   (:refer-clojure :exclude [compile])
-  (:import [java.nio.file Paths Files StandardOpenOption OpenOption]
+  (:import [java.nio.file Path Paths Files StandardOpenOption OpenOption]
            [java.nio.file.attribute FileAttribute]
-           [java.io FileInputStream DataInputStream ByteArrayInputStream]
+           [java.io File FileInputStream DataInputStream ByteArrayInputStream]
            [java.nio ByteBuffer ByteOrder]
-           [java.nio.channels FileChannel]))
+           [java.nio.channels FileChannel]
+           [java.net URL URI URLClassLoader]))
 
 (comment
   (def OPEN_OPTIONS (doto (make-array OpenOption 1)
@@ -113,9 +114,9 @@
         (finally (.close file-channel)))))
   )
 
-(defn compile
+(defn do-compile
   ([namespaces]
-   (compile namespaces nil))
+   (do-compile namespaces nil))
   ([namespaces {:keys [compile-path compiler-options]}]
    (let [namespaces (if (coll? namespaces)
                       namespaces
@@ -130,12 +131,61 @@
        (doseq [namespace namespaces]
          (clojure.core/compile namespace))))))
 
-(comment
-  (compile 'badigeon.compile)
-  )
+(defn classpath->paths [classpath]
+  (when classpath
+    (for [path (-> classpath
+                   clojure.string/trim
+                   (.split File/pathSeparator))]
+      (Paths/get path (make-array String 0)))))
 
+(defn paths->urls [paths]
+  (->> paths
+       (map #(.toUri ^Path %))
+       (map #(.toURL ^URI %))))
+
+(defn -main [namespaces options]
+  (let [namespaces (read-string namespaces)
+        options (read-string options)]
+    (do-compile namespaces options)
+    (clojure.core/shutdown-agents)))
+
+(defn compile
+  ([namespaces]
+   (compile namespaces nil))
+  ([namespaces options]
+   (let [classpath (System/getProperty "java.class.path")
+         classpath-urls (->> classpath classpath->paths paths->urls (into-array URL))
+         classloader (URLClassLoader. classpath-urls
+                                      (.getParent (ClassLoader/getSystemClassLoader)))
+         main-class (.loadClass classloader "clojure.main")
+         main-method (.getMethod
+                      main-class "main"
+                      (into-array Class [(Class/forName "[Ljava.lang.String;")]))
+         t (Thread. (fn []
+                      (.setContextClassLoader (Thread/currentThread) classloader)
+                      (.invoke
+                       main-method
+                       nil
+                       (into-array
+                        Object [(into-array String ["--main"
+                                                    "badigeon.compile"
+                                                    (pr-str namespaces)
+                                                    (pr-str options)])]))))]
+     (.start t)
+     (.join t)
+     (.close classloader))))
+
+(comment
+  
+  (compile '[badigeon.main] {:compile-path "target/classes"
+                             :compiler-options {:elide-meta [:doc :file :line :added]}})
+  
+  )
 
 ;; Cleaning non project classes: https://dev.clojure.org/jira/browse/CLJ-322
 
 ;; Cleaning non project classes is not supported by badigeon because:
 ;; Most of the time, libraries should be shipped without AOT. In the rare case when a library must be shipped AOT (let's say we don't want to ship the sources), directories can be removed programmatically, between build tasks. Shipping an application with AOT is a more common use case. In this case, AOT compiling dependencies is not an issue.
+
+;; Compiling is done in a separate classloader because
+;; - clojure.core/compile recursively compiles a namespace and its dependencies, unless the dependencies are already loaded. :reload-all does not help. Removing the AOT compiled files and recompiling results in a strange result: Source files are not reloaded, no .class file is produced. Using a separate classloader simulates a :reload-all for compile. 
