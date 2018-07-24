3dc040d2476a03813da4f907a80648f120bcea27
ewen <ewen.grosjean@free.fr>
2018-07-24 23:11:06 +0200

    zip comments + optional argument

--- src/badigeon/zip.clj
+++ src/badigeon/zip.clj
@@ -25,22 +25,27 @@
             FileVisitResult/SKIP_SUBTREE
             :else (throw exception)))))
 
-(defn zip [directory-path out-path]
-  "Zip a directory. Outputs the zipped directory to \"out-path\". The directory to be zipped is often the directory created by the Badigeon \"bundle\" function."
-  (let [directory-path (if (string? directory-path)
-                         (Paths/get directory-path (make-array String 0))
-                         directory-path)
-        out-path (if (string? out-path)
-                   (Paths/get out-path (make-array String 0))
-                   out-path)]
-    (with-open [zip-out (-> (.toFile ^Path out-path)
-                            (FileOutputStream.)
-                            (BufferedOutputStream.)
-                            (ZipOutputStream.))]
-      (Files/walkFileTree directory-path
-                          (EnumSet/of FileVisitOption/FOLLOW_LINKS)
-                          Integer/MAX_VALUE
-                          (make-file-visitor directory-path zip-out))
-      (str out-path))))
+(defn zip
+  "Zip a directory. By default, outputs the zipped directory to a file with the same name than \"directory-path\" but with a .zip extension. The directory to be zipped is often the directory created by the Badigeon \"bundle\" function."
+  ([directory-path]
+   (zip directory-path nil))
+  ([directory-path out-path]
+   (let [directory-path (if (string? directory-path)
+                          (Paths/get directory-path (make-array String 0))
+                          directory-path)
+         out-path (if (string? out-path)
+                    (Paths/get out-path (make-array String 0))
+                    out-path)
+         out-path (or out-path (.resolveSibling
+                                directory-path (str (.getFileName directory-path) ".zip")))]
+     (with-open [zip-out (-> (.toFile ^Path out-path)
+                             (FileOutputStream.)
+                             (BufferedOutputStream.)
+                             (ZipOutputStream.))]
+       (Files/walkFileTree directory-path
+                           (EnumSet/of FileVisitOption/FOLLOW_LINKS)
+                           Integer/MAX_VALUE
+                           (make-file-visitor directory-path zip-out))
+       (str out-path)))))
 
 
