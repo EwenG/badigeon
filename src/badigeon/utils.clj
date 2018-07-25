d1175380a680dc0667fb085b8b7eed74f6760bdb
ewen <ewen.grosjean@free.fr>
2018-07-25 23:45:18 +0200

    0.0.2-SNAPSHOT

--- src/badigeon/utils.clj
+++ src/badigeon/utils.clj
@@ -1,7 +1,7 @@
 (ns badigeon.utils
   (:import [java.nio.file Paths Path]))
 
-(def ^:const version "0.0.1")
+  (def ^:const version "0.0.2-SNAPSHOT")
 
 (defn relativize-path [^Path root-path ^Path path]
   (if (= root-path path)
