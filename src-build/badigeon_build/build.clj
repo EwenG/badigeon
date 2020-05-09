84f4285853a31b99bd292eae897bd8bbd0f994a9
ewen <ewen.grosjean@free.fr>
2020-05-09 13:58:34 +0200

    deploy script

--- src-build/badigeon_build/build.clj
+++ src-build/badigeon_build/build.clj
@@ -1,7 +1,22 @@
 (ns badigeon-build-build
   (:require [badigeon.jar :as jar]
-            [badigeon.install :as install]))
+            [badigeon.install :as install]
+            [badigeon.utils :as utils]
+            [badigeon.sign :as sign]
+            [badigeon.deploy :as deploy]
+            [badigeon.prompt :as prompt]))
 
 (defn -main []
-  (jar/jar 'badigeon/badigeon {:mvn/version badigeon.utils/version}
-           {:paths ["src"]}))
+  (let [jar-path (jar/jar 'badigeon/badigeon {:mvn/version badigeon.utils/version}
+                          {:paths ["src"]})
+        artifacts [{:file-path jar-path}
+                   {:file-path "pom.xml"}]
+        artifacts (sign/sign artifacts)
+        password (prompt/prompt-password "Password: ")]
+    (deploy/deploy
+     'badigeon/badigeon badigeon.utils/version
+     artifacts
+     {:id "clojars"
+      :url "https://repo.clojars.org/"}
+     {:credentials {:username "ewen" :password password}})))
+
