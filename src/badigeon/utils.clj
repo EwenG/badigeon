9c84f3172e20454a3f0a12581dba6d500c58c270
ewen <ewen.grosjean@free.fr>
2020-07-13 11:04:09 +0200

    1.1-SNAPSHOT

--- src/badigeon/utils.clj
+++ src/badigeon/utils.clj
@@ -2,7 +2,7 @@
   (:require [clojure.tools.deps.alpha.util.maven :as maven])
   (:import [java.nio.file Paths Path StandardCopyOption]))
 
-(def ^:const version "1.0")
+(def ^:const version "1.1-SNAPSHOT")
 
 (def ^"[Ljava.nio.file.StandardCopyOption;" copy-options
   (into-array StandardCopyOption
