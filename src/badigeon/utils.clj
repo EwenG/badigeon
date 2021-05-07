6928878a40ee100caa9816a25078593e68f4bc2d
ewen <ewen.grosjean@free.fr>
2021-05-07 15:31:13 +0200

    1.7-SNAPSHOT

--- src/badigeon/utils.clj
+++ src/badigeon/utils.clj
@@ -5,7 +5,7 @@
            [java.util.jar JarEntry JarOutputStream]
            [java.util.zip ZipEntry ZipOutputStream]))
 
-(def ^:const version "1.6")
+(def ^:const version "1.7-SNAPSHOT")
 
 (def ^"[Ljava.nio.file.StandardCopyOption;" copy-options
   (into-array StandardCopyOption
